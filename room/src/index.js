0ae697c9dc5ea72a4886b8e4a8bac226fa43fb54
Ahmad-3weeda <3weeda@gmail.com>
2019-02-07 14:04:28 +0200

    Adding Redux-persist

--- room/src/index.js
+++ room/src/index.js
@@ -1,28 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import storage from 'redux-persist/lib/storage'
+import './index.css';
+import App from './App';
 import room from './store/reducers/room';
+import Spinner from './UI/Spinner/Spinner';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const rootReducer = combineReducers({
     room: room
 })
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)))
+const persistor = persistStore(store)
+
 const app = (
     <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <PersistGate loading={<Spinner />} persistor={persistor}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </PersistGate>
     </Provider>
 )
+
 
 ReactDOM.render(app, document.getElementById('root'));
 
