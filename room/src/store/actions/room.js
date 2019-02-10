2fa16fc6bbd71df752767b3f34beeda0e5b36d0f
Ahmad-3weeda <3weeda@gmail.com>
2019-02-10 20:48:49 +0200

    Room actions file

---
+++ room/src/store/actions/room.js
@@ -0,0 +1,13 @@
+import * as actionTypes from './actionTypes';
+
+export const zoomIn = () => {
+    return {
+        type: actionTypes.ZOOM_IN
+    }
+}
+export const zoomOut = () => {
+    return {
+        type: actionTypes.ZOOM_OUT
+    }
+}
+
