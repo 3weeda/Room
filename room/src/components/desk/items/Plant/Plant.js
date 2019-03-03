0803ce710899811709ce9d87c8bb19ffb031230b
Ahmad-3weeda <3weeda@gmail.com>
2019-03-03 19:01:33 +0200

    Adding modal to Plant

--- room/src/components/desk/items/Plant/Plant.js
+++ room/src/components/desk/items/Plant/Plant.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactSVG from 'react-svg';
 import svg from '../../../../assets/svg/10-plant.svg';
 import classes from './Plant.css'
+import Modal from '../../../../UI/Modal/Modal';
+import Auxiliary from '../../../../hoc/Auxiliary/Auxiliary';
 
-const Plant = () => (
-    <div className={classes.box}>
-        <ReactSVG
-            src={svg}
-            svgStyle={{ width: 135 }} />
-    </div>
+const Plant = (props) => (
+    <Auxiliary>
+        <div className={classes.Plant} onClick={props.zoomIn}>
+            <ReactSVG
+                src={svg}
+                svgStyle={{ width: 135 }} />
+        </div>
+        <Modal
+            visible={props.elementZoomed}
+            closeModal={props.zoomOut}
+            nightMode="true"
+            width="600px"
+            left="calc(50% - 300px)"
+        >
+            <div style={{ color: 'white' }}><p>Plant</p></div>
+        </Modal>
+    </Auxiliary>
 );
 export default Plant;
