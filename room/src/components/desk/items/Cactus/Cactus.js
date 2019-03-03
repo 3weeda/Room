d5a3be77e47e00d250cae4249c703312f3a2cc01
Ahmad-3weeda <3weeda@gmail.com>
2019-03-03 18:59:34 +0200

    Adding modal to cactus

--- room/src/components/desk/items/Cactus/Cactus.js
+++ room/src/components/desk/items/Cactus/Cactus.js
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactSVG from 'react-svg';
 import svg from '../../../../assets/svg/14-cactus.svg';
 import classes from './Cactus.css'
+import Modal from '../../../../UI/Modal/Modal';
 
-const Cactus = () => (
-    <div className={classes.Cactus}>
-        <ReactSVG
-            src={svg}
-            svgStyle={{ width: "45" }} />
+const Cactus = (props) => (
+    <div className={classes.box}>
+        <div className={classes.Cactus} onClick={props.zoomIn}>
+            <ReactSVG
+                src={svg}
+                svgStyle={{ width: "45" }} />
+        </div>
+        <Modal
+            visible={props.elementZoomed}
+            closeModal={props.zoomOut}
+            nightMode="true"
+            width="600px"
+            left="calc(50% - 300px)"
+        >
+            <div style={{color: 'white'}}><p>Cactus</p></div>
+        </Modal>
     </div>
 );
 export default Cactus;
