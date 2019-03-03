6bab3406ab57fcaee6030f03404395667e831d1d
Ahmad-3weeda <3weeda@gmail.com>
2019-03-03 19:02:45 +0200

    Adding modal to Window

--- room/src/components/desk/items/Window/Window.js
+++ room/src/components/desk/items/Window/Window.js
@@ -2,13 +2,26 @@ import React from 'react';
 import ReactSVG from 'react-svg';
 import svg from '../../../../assets/svg/18-window.svg';
 import classes from './Window.css'
+import Auxiliary from '../../../../hoc/Auxiliary/Auxiliary';
+import Modal from '../../../../UI/Modal/Modal';
 
-const Windo = () => (
-    <div className={classes.box}>
-        <ReactSVG
-            src={svg}
-            svgStyle={{ width: 250 }}
-            svgClassName={classes.Window}/>
-    </div>
+const Windo = (props) => (
+    <Auxiliary>
+        <div className={classes.Windo} onClick={props.zoomIn}>
+            <ReactSVG
+                src={svg}
+                svgStyle={{ width: 250 }}
+                svgClassName={classes.Window} />
+        </div>
+        <Modal
+            visible={props.elementZoomed}
+            closeModal={props.zoomOut}
+            nightMode={props.nightMode}
+            width="800px"
+            left="calc(50% - 400px)"
+        >
+            <div style={{color: 'white'}}><p>Window</p></div>
+        </Modal>
+    </Auxiliary>
 );
 export default Windo;
