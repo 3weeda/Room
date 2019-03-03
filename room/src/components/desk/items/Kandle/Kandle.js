d46decbcd5e2c64e4909841adf690e04a6e36623
Ahmad-3weeda <3weeda@gmail.com>
2019-03-03 19:01:23 +0200

    Adding modal to Kandle

--- room/src/components/desk/items/Kandle/Kandle.js
+++ room/src/components/desk/items/Kandle/Kandle.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactSVG from 'react-svg';
 import svg from '../../../../assets/svg/5-kandle.svg';
 import classes from './Kandle.css'
+import Modal from '../../../../UI/Modal/Modal';
+import Auxiliary from '../../../../hoc/Auxiliary/Auxiliary';
 
-const Kandle = () => (
-    <div className={classes.box}>
-        <ReactSVG
-            src={svg}
-            svgStyle={{ width: 35 }} />
-    </div>
+const Kandle = (props) => (
+    <Auxiliary>
+        <div className={classes.Kandle} onClick={props.zoomIn}>
+            <ReactSVG
+                src={svg}
+                svgStyle={{ width: 35 }} />
+        </div>
+        <Modal
+            visible={props.elementZoomed}
+            closeModal={props.zoomOut}
+            nightMode="true"
+            width="600px"
+            left="calc(50% - 300px)"
+        >
+            <div style={{ color: 'white' }}><p>Kandle</p></div>
+        </Modal>
+    </Auxiliary>
 );
 export default Kandle;
