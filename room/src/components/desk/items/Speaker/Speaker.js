e3d7e9ff4b7826209d3db2e23fbc246871af6437
Ahmad-3weeda <3weeda@gmail.com>
2019-03-03 19:05:51 +0200

    Adding modal to speakers

--- room/src/components/desk/items/Speaker/Speaker.js
+++ room/src/components/desk/items/Speaker/Speaker.js
@@ -3,19 +3,29 @@ import ReactSVG from 'react-svg';
 import svg from '../../../../assets/svg/16-speaker.svg';
 import classes from './Speaker.css'
 import Auxiliary from '../../../../hoc/Auxiliary/Auxiliary';
+import Modal from '../../../../UI/Modal/Modal';
 
-const Speaker = () => (
+const Speaker = (props) => (
     <Auxiliary>
-        <div className={classes.box1}>
+        <div className={classes.box1} onClick={props.zoomIn}>
             <ReactSVG
                 src={svg}
-                svgStyle={{ width: 45 }}/>
+                svgStyle={{ width: 45 }} />
         </div>
-        <div className={classes.box2}>
+        <div className={classes.box2} onClick={props.zoomIn}>
             <ReactSVG
                 src={svg}
-                svgStyle={{ width: 45 }}/>
+                svgStyle={{ width: 45 }} />
         </div>
+        <Modal
+            visible={props.elementZoomed}
+            closeModal={props.zoomOut}
+            nightMode="true"
+            width="600px"
+            left="calc(50% - 300px)"
+        >
+            <div style={{color: 'white'}}><p>Speakers</p></div>
+        </Modal>
     </Auxiliary>
 );
 export default Speaker;
