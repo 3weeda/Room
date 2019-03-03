3cab8f7e61947f080aab4463d343c62bf6ac219e
Ahmad-3weeda <3weeda@gmail.com>
2019-03-03 19:02:15 +0200

    Adding modal to vase

--- room/src/components/desk/items/Vase/Vase.js
+++ room/src/components/desk/items/Vase/Vase.js
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactSVG from 'react-svg';
 import svg from '../../../../assets/svg/4-vase.svg';
 import classes from './Vase.css'
+import Modal from '../../../../UI/Modal/Modal';
+import Auxiliary from '../../../../hoc/Auxiliary/Auxiliary';
 
-const Vase = () => (
-    <div className={classes.box}>
-        <ReactSVG
-            src={svg}
-            svgStyle={{ width: 75 }} />
-    </div>
+const Vase = (props) => (
+    <Auxiliary>
+        <div className={classes.Vase} onClick={props.zoomIn}>
+            <ReactSVG
+                src={svg}
+                svgStyle={{ width: 75 }} />
+        </div>
+        <Modal
+            visible={props.elementZoomed}
+            closeModal={props.zoomOut}
+            nightMode="true"
+            width="600px"
+            left="calc(50% - 300px)"
+        >
+            <div style={{ color: 'white' }}><p>Vase</p></div>
+        </Modal>
+
+    </Auxiliary>
 );
 export default Vase;
