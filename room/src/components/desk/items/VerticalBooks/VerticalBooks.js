8eb1536ff5e6dc768ff3e6f150c7bf91907fe72e
Ahmad-3weeda <3weeda@gmail.com>
2019-03-03 19:02:32 +0200

    Adding modal to VerticalBooks

--- room/src/components/desk/items/VerticalBooks/VerticalBooks.js
+++ room/src/components/desk/items/VerticalBooks/VerticalBooks.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactSVG from 'react-svg';
 import svg from '../../../../assets/svg/6-verticleBooks.svg';
 import classes from './VerticalBooks.css'
+import Modal from '../../../../UI/Modal/Modal';
+import Auxiliary from '../../../../hoc/Auxiliary/Auxiliary';
 
-const VerticalBooks = () => (
-    <div className={classes.box}>
-        <ReactSVG
-            src={svg}
-            svgStyle={{ width: 60 }} />
-    </div>
+const VerticalBooks = (props) => (
+    <Auxiliary>
+        <div className={classes.VerticalBooks} onClick={props.zoomIn}>
+            <ReactSVG
+                src={svg}
+                svgStyle={{ width: 60 }} />
+        </div>
+        <Modal
+            visible={props.elementZoomed}
+            closeModal={props.zoomOut}
+            nightMode="true"
+            width="600px"
+            left="calc(50% - 300px)"
+        >
+            <div style={{ color: 'white' }}><p>Shelf Books</p></div>
+        </Modal>
+    </Auxiliary>
 );
 export default VerticalBooks;
