2820a1b72f5fd6a516c5a9fefcc9cea25d8be394
Ahmad-3weeda <3weeda@gmail.com>
2019-03-03 19:03:37 +0200

    Setting up room modal actions in redux

--- room/src/store/actions/room.js
+++ room/src/store/actions/room.js
@@ -1,13 +1,57 @@
 import * as actionTypes from './actionTypes';
 
-export const zoomIn = () => {
+export const clockZoomIn = () => {
     return {
-        type: actionTypes.ZOOM_IN
+        type: actionTypes.CLOCK_ZOOM_IN,
+    }
+}
+export const computerZoomIn = () => {
+    return {
+        type: actionTypes.COMPUTER_ZOOM_IN,
+    }
+}
+export const vaseZoomIn = () => {
+    return {
+        type: actionTypes.VASE_ZOOM_IN,
+    }
+}
+export const kandleZoomIn = () => {
+    return {
+        type: actionTypes.KANDLE_ZOOM_IN,
+    }
+}
+export const shelfBooksZoomIn = () => {
+    return {
+        type: actionTypes.SHELFBOOKS_ZOOM_IN,
+    }
+}
+export const cactusZoomIn = () => {
+    return {
+        type: actionTypes.CACTUS_ZOOM_IN,
+    }
+}
+export const speakerZoomIn = () => {
+    return {
+        type: actionTypes.SPEAKER_ZOOM_IN,
+    }
+}
+export const deskBooksZoomIn = () => {
+    return {
+        type: actionTypes.DESKBOOKS_ZOOM_IN,
+    }
+}
+export const plantZoomIn = () => {
+    return {
+        type: actionTypes.PLANT_ZOOM_IN,
+    }
+}
+export const windowZoomIn = () => {
+    return {
+        type: actionTypes.WINDOW_ZOOM_IN,
     }
 }
 export const zoomOut = () => {
     return {
-        type: actionTypes.ZOOM_OUT
+        type: actionTypes.ZOOM_OUT,
     }
 }
-
