0d4b1ce3a98ff0ba322beb4b2f05d2e1b9471351
Ahmad-3weeda <3weeda@gmail.com>
2019-03-03 19:04:16 +0200

    room action creators

--- room/src/store/actions/actionTypes.js
+++ room/src/store/actions/actionTypes.js
@@ -8,5 +8,14 @@ export const SIGNUP_ON = 'SIGNUP_ON';
 export const LOGIN_ON = 'LOGIN_ON';
 export const MODAL_OFF = 'MODAL_OFF';
 
-export const ZOOM_IN = 'ZOOM_IN';
+export const CLOCK_ZOOM_IN = 'CLOCK_ZOOM_IN';
+export const COMPUTER_ZOOM_IN = 'COMPUTER_ZOOM_IN';
+export const VASE_ZOOM_IN = 'VASE_ZOOM_IN';
+export const KANDLE_ZOOM_IN = 'KANDLE_ZOOM_IN';
+export const SHELFBOOKS_ZOOM_IN = 'SHELFBOOKS_ZOOM_IN';
+export const CACTUS_ZOOM_IN = 'CACTUS_ZOOM_IN';
+export const SPEAKER_ZOOM_IN = 'SPEAKER_ZOOM_IN';
+export const DESKBOOKS_ZOOM_IN = 'DESKBOOKS_ZOOM_IN';
+export const PLANT_ZOOM_IN = 'PLANT_ZOOM_IN';
+export const WINDOW_ZOOM_IN = 'WINDOW_ZOOM_IN';
 export const ZOOM_OUT = 'ZOOM_OUT';
--- room/src/store/actions/index.js
+++ room/src/store/actions/index.js
@@ -12,6 +12,15 @@ export {
 } from './auth'
 
 export {
-    zoomIn,
+    clockZoomIn,
+    computerZoomIn,
+    vaseZoomIn,
+    kandleZoomIn,
+    shelfBooksZoomIn,
+    cactusZoomIn,
+    speakerZoomIn,
+    deskBooksZoomIn,
+    plantZoomIn,
+    windowZoomIn,
     zoomOut
 } from './room'
