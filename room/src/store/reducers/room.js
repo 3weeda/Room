075a409805b4216b45a46828b4cffa09bad5065f
Ahmad-3weeda <3weeda@gmail.com>
2019-03-03 19:04:32 +0200

    room reducer

--- room/src/store/reducers/room.js
+++ room/src/store/reducers/room.js
@@ -2,16 +2,31 @@ import * as actionTypes from '../actions/actionTypes';
 import { updateObject } from '../../Shared/utility';
 
 const initialState = {
-    elementZoomed: false
+    clock: false,
+    computer: false,
+    vase: false,
+    kandle: false,
+    shelfBooks: false,
+    cactus: false,
+    speaker: false,
+    deskBooks: false,
+    plant: false,
+    window: false
 }
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.ZOOM_IN:
-        return updateObject(state, { elementZoomed: true })
-        case actionTypes.ZOOM_OUT:
-        return updateObject(state, { elementZoomed: false })
-        default:
-            return state;
+        case actionTypes.CLOCK_ZOOM_IN: return updateObject(state, { clock: true })
+        case actionTypes.COMPUTER_ZOOM_IN: return updateObject(state, { computer: true })
+        case actionTypes.VASE_ZOOM_IN: return updateObject(state, { vase: true })
+        case actionTypes.KANDLE_ZOOM_IN: return updateObject(state, { kandle: true })
+        case actionTypes.SHELFBOOKS_ZOOM_IN: return updateObject(state, { shelfBooks: true })
+        case actionTypes.CACTUS_ZOOM_IN: return updateObject(state, { cactus: true })
+        case actionTypes.SPEAKER_ZOOM_IN: return updateObject(state, { speaker: true })
+        case actionTypes.DESKBOOKS_ZOOM_IN: return updateObject(state, { deskBooks: true })
+        case actionTypes.PLANT_ZOOM_IN: return updateObject(state, { plant: true })
+        case actionTypes.WINDOW_ZOOM_IN: return updateObject(state, { window: true })
+        case actionTypes.ZOOM_OUT: return updateObject(initialState)
+        default: return state;
     }
 }
 
