4d9e149078dcc9361d728ce8b50345543eb1c2e5
Ahmad-3weeda <3weeda@gmail.com>
2019-04-05 17:47:07 +0200

    Edit css for night mode toggle button

--- room/src/hoc/Layout/nightModeToggle/NightModeToggle.js
+++ room/src/hoc/Layout/nightModeToggle/NightModeToggle.js
@@ -3,10 +3,10 @@ import classes from './NightModeToggle.css';
 
 const NightModeToggle = (props) => {
     return (
-        <div className={classes.toggleWrapper} title="Night Mode">
+        <div className={[classes.toggleWrapper, props.className].join(" ")} title="Night Mode">
             <input type="checkbox" className={classes.dn} id="dn" onChange={props.clicked}  checked={props.nightMode} />
             <label htmlFor="dn" className={classes.toggle}>
-                <span className={classes.toggle__handler}>
+                <span className={[classes.toggle__handler, props.toggleClassName].join(" ")}>
                     <span className={[classes.crater, classes.crater1].join(' ')}></span>
                     <span className={[classes.crater, classes.crater2].join(' ')}></span>
                     <span className={[classes.crater, classes.crater3].join(' ')}></span>
