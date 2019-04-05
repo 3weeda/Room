30f54f328c68a18257ae647ee5a6861074602208
Ahmad-3weeda <3weeda@gmail.com>
2019-04-05 17:56:37 +0200

    Adding Google book library to Vertical books

--- room/src/components/desk/items/VerticalBooks/VerticalBooks.js
+++ room/src/components/desk/items/VerticalBooks/VerticalBooks.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import ReactSVG from 'react-svg';
-import svg from '../../../../assets/svg/6-verticleBooks.svg';
-import classes from './VerticalBooks.css'
-import Modal from '../../../../UI/Modal/Modal';
-import Auxiliary from '../../../../hoc/Auxiliary/Auxiliary';
+import React from "react";
+import ReactSVG from "react-svg";
+import svg from "../../../../assets/svg/6-verticleBooks.svg";
+import classes from "./VerticalBooks.css";
+import Modal from "../../../../UI/Modal/Modal";
+import Auxiliary from "../../../../hoc/Auxiliary/Auxiliary";
+import BookLibrary from "./BookLibrary/BookLibrary";
 
-const VerticalBooks = (props) => (
-    <Auxiliary>
-        <div className={classes.VerticalBooks} onClick={props.zoomIn}>
-            <ReactSVG
-                src={svg}
-                svgStyle={{ width: 60 }} />
-        </div>
-        <Modal
-            visible={props.elementZoomed}
-            closeModal={props.zoomOut}
-            nightMode="true"
-            width="600px"
-            left="calc(50% - 300px)"
-        >
-            <div style={{ color: 'white' }}><p>Shelf Books</p></div>
-        </Modal>
-    </Auxiliary>
+const VerticalBooks = props => (
+  <Auxiliary>
+    <div className={classes.VerticalBooks} onClick={props.zoomIn}>
+      <ReactSVG src={svg} svgStyle={{ width: 60 }} />
+    </div>
+    <Modal
+      visible={props.elementZoomed}
+      closeModal={props.zoomOut}
+      nightMode="true"
+      width="600px"
+      left="calc(50% - 300px)"
+    >
+      <div style={{ color: "white" }}>
+        <p>Shelf Books</p>
+        <BookLibrary />
+        {/* key: x9Mns1ayA7AWF4jBG5jAlw
+        secret: PGC6d02tSDwvYvIRtg3qe1vQ4HP437OLAavBirukDEA */}
+
+      </div>
+    </Modal>
+  </Auxiliary>
 );
 export default VerticalBooks;
