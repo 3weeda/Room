4fcf6f0f53623c96b78bbb852c4276df9603d8a0
Ahmad-3weeda <3weeda@gmail.com>
2019-04-05 17:58:56 +0200

    Prettier on Stopwatch app

--- room/src/components/desk/items/Clock/Stopwatch/Stopwatch.js
+++ room/src/components/desk/items/Clock/Stopwatch/Stopwatch.js
@@ -1,95 +1,128 @@
-import React from 'react';
-import classes from './Stopwatch.css'
+import React from "react";
+import classes from "./Stopwatch.css";
 
-
-const formattedSeconds = (sec) =>
-    Math.floor(sec / 60) +
-    ':' +
-    ('0' + sec % 60).slice(-2)
-
+const formattedSeconds = sec =>
+  Math.floor(sec / 60) + ":" + ("0" + (sec % 60)).slice(-2);
 
 class Stopwatch extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            secondsElapsed: 0,
-            laps: [],
-            lastClearedIncrementer: null
-        };
-        this.incrementer = null;
-    }
+  constructor(props) {
+    super(props);
+    this.handleStartClick = this.handleStartClick.bind(this);
+    this.state = {
+      secondsElapsed: 0,
+      laps: [],
+      lastClearedIncrementer: null,
+      swap: false
+    };
+    this.incrementer = null;
+  }
 
-    handleStartClick() {
-        this.incrementer = setInterval(() =>
-            this.setState({
-                secondsElapsed: this.state.secondsElapsed + 1
-            })
-            , 1000);
-    }
-
-    handleStopClick() {
-        clearInterval(this.incrementer);
+  handleStartClick() {
+    this.setState({ swap: true });
+    this.incrementer = setInterval(
+      () =>
         this.setState({
-            lastClearedIncrementer: this.incrementer
-        });
+          secondsElapsed: this.state.secondsElapsed + 1
+        }),
+      1000
+    );
+  }
+
+  handleStopClick() {
+    clearInterval(this.incrementer);
+    this.setState({
+      lastClearedIncrementer: this.incrementer,
+      swap: false
+    });
+  }
+
+  handleResetClick() {
+    clearInterval(this.incrementer);
+    this.setState({
+      secondsElapsed: 0,
+      laps: [],
+      swap: false
+    });
+  }
+
+  handleLabClick() {
+    this.setState({
+      laps: this.state.laps.concat([this.state.secondsElapsed])
+    });
+  }
+  // Cannot read property 'handleStartClick' of undefined
+  onKeyDown(e) {
+    if (e.key === "Enter") {
+      this.handleStartClick();
     }
+  }
 
-    handleResetClick() {
-        clearInterval(this.incrementer);
-        this.setState({
-            secondsElapsed: 0,
-            laps: []
-        });
-    }
+  render() {
+    return (
+      <div className={this.props.visible ? classes.Stopwatch : classes.hide}>
+        <h1 className={classes.stopwatchTimer}>
+          {formattedSeconds(this.state.secondsElapsed)}
+        </h1>
 
-    handleLabClick() {
-        this.setState({
-            laps: this.state.laps.concat([this.state.secondsElapsed])
-        })
-    }
+        {!this.state.swap &&
+        (this.state.secondsElapsed === 0 ||
+        this.incrementer === this.state.lastClearedIncrementer) ? (
+          <Button
+            className={[classes.Btn, classes.startBtn].join(" ")}
+            onClick={this.handleStartClick}
+            // onKeyDown={this.onKeyDown}
+          >
+            {this.state.secondsElapsed === 0 ? 'start' : 'resume'}
+          </Button>
+        ) : (
+          <Button
+            className={[classes.Btn, classes.stopBtn].join(" ")}
+            onClick={this.handleStopClick.bind(this)}
+          >
+            stop
+          </Button>
+        )}
+        {this.state.swap ||
+        (this.state.secondsElapsed !== 0 &&
+          this.incrementer !== this.state.lastClearedIncrementer) ? (
+          <Button
+            className={classes.Btn}
+            onClick={this.handleLabClick.bind(this)}
+          >
+            lab
+          </Button>
+        ) : null}
 
-    render() {
-        return (
-            <div className={this.props.visible ? classes.Stopwatch : classes.hide}>
-                <h1 className={classes.stopwatchTimer}>{formattedSeconds(this.state.secondsElapsed)}</h1>
+        {this.state.swap ||
+        (this.state.secondsElapsed !== 0 &&
+          this.incrementer === this.state.lastClearedIncrementer) ? (
+          <Button
+            className={classes.Btn}
+            onClick={this.handleResetClick.bind(this)}
+          >
+            reset
+          </Button>
+        ) : null}
 
-                {(this.state.secondsElapsed === 0 ||
-                    this.incrementer === this.state.lastClearedIncrementer
-                    ? <Button
-                        className={[classes.Btn, classes.startBtn].join(' ')}
-                        onClick={this.handleStartClick.bind(this)}>start</Button>
-                    : <Button
-                        className={[classes.Btn, classes.stopBtn].join(' ')}
-                        onClick={this.handleStopClick.bind(this)}>stop</Button>
-                )}
-                {(this.state.secondsElapsed !== 0 &&
-                    this.incrementer !== this.state.lastClearedIncrementer
-                    ? <Button
-                        className={classes.Btn}
-                        onClick={this.handleLabClick.bind(this)}>lab</Button>
-                    : null
-                )}
-
-
-                {(this.state.secondsElapsed !== 0 &&
-                    this.incrementer === this.state.lastClearedIncrementer
-                    ? <Button
-                        className={classes.Btn}
-                        onClick={this.handleResetClick.bind(this)}>reset</Button>
-                    : null
-                )}
-
-                <ul className={classes.stopwatchLaps}>
-                    {this.state.laps.map((lap, i) =>
-                        <li className={classes.stopwatchLap}><strong>{i + 1}:</strong> {formattedSeconds(lap)}</li>)
-                    }
-                </ul>
-            </div>
-        );
-    }
+        <ul className={classes.stopwatchLaps}>
+          {this.state.laps.map((lap, i) => (
+            <li className={classes.stopwatchLap} key={i}>
+              <strong>{i + 1}:</strong> {formattedSeconds(lap)}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-const Button = (props) =>
-    <button type="button" {...props} className={props.className} />;
+const Button = props => (
+  <button
+    type="button"
+    {...props}
+    className={props.className}
+    onKeyDown={props.onKeyDown}
+  />
+);
 
 export default Stopwatch;
