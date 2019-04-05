38f7185857a3d60a5f98f15dc28623a2efcc4b13
Ahmad-3weeda <3weeda@gmail.com>
2019-04-05 18:31:55 +0200

    Editing Routes accessability onAuthenticaiton

--- room/src/App.js
+++ room/src/App.js
@@ -1,27 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { StyleRoot } from 'radium';
-import Room from './containers/Room/Room';
-import Home from './containers/Home/Home';
-import About from './components/About/About';
-import Plans from './containers/Plans/Plans';
-import HowItWorks from './containers/HowItWorks/HowItWorks';
+import React, { Component } from "react";
+import "./App.css";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { StyleRoot } from "radium";
+import Room from "./containers/Room/Room";
+import Home from "./containers/Home/Home";
+import About from "./components/About/About";
+import Plans from "./containers/Plans/Plans";
+import HowItWorks from "./containers/HowItWorks/HowItWorks";
+import Auxiliary from "./hoc/Auxiliary/Auxiliary";
 
 class App extends Component {
-
   render() {
     return (
       <StyleRoot>
         <div className="App">
           <Switch>
-            <Route path="/tour" component={HowItWorks} />
-            {this.props.isAuthenticated ? <Route path="/room" component={Room} /> : null}
-            <Route path="/about" component={About} />
-            <Route path="/plans" component={Plans} />
-            <Route path="/" exact component={Home} />
-            <Redirect to="/" />
+            {/* TODO: this is temp solution for showing room on logging in */}
+            {this.props.isAuthenticated ? (
+              <Route path="/room" component={Room} />
+            ) : (
+              <Auxiliary>
+                <Route path="/tour" component={HowItWorks} />
+                <Route path="/about" component={About} />
+                <Route path="/plans" component={Plans} />
+                <Route path="/" exact component={Home} />
+                <Redirect to="/" />
+              </Auxiliary>
+            )}
+            {this.props.isAuthenticated ? <Redirect to="/room" /> : null}
           </Switch>
         </div>
       </StyleRoot>
@@ -32,7 +39,7 @@ class App extends Component {
 const mapStateToProps = state => {
   return {
     isAuthenticated: state.auth.token !== null
-  }
-}
+  };
+};
 
 export default withRouter(connect(mapStateToProps)(App));
