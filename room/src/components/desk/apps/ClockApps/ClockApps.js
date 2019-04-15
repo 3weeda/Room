b86ce9ca3af19d114382ad711e8abf1629e103f3
Ahmad-3weeda <3weeda@gmail.com>
2019-04-15 21:18:27 +0200

    using Applications hoc in Clock

--- room/src/components/desk/apps/ClockApps/ClockApps.js
+++ room/src/components/desk/apps/ClockApps/ClockApps.js
@@ -4,7 +4,14 @@ import Alarm from "../../items/Clock/Alarm/Alarm";
 import Stopwatch from "../../items/Clock/Stopwatch/Stopwatch";
 import Timer from "../../items/Clock/Timer/Timer";
 import Clock from "../../items/Clock/Clock";
-import Backdrop from "../../../../UI/Backdrop/Backdrop";
+import Applications from "../../../../hoc/Applications/Applications"
+import Auxiliary from "../../../../hoc/Auxiliary/Auxiliary";
+
+// const apps = [
+//   { app: "Alarm", state: "alarm" },
+//   { app: "Stopw", state: "stopwatch" },
+//   { app: "Timer", state: "timer" },
+// ];
 
 class ClockApps extends React.Component {
   state = {
@@ -34,62 +41,65 @@ class ClockApps extends React.Component {
   }
 
   render() {
+    const appSvg = (
+      <Auxiliary>
+        <Clock
+          size={700}
+          hourFormat="roman"
+          timeFormat="standard"
+          boxClassName={classes.ClockBox}
+          className={classes.Clock}
+        />
+        <div className={classes.Time}>
+          <h2>{this.state.currentTime}</h2>
+        </div>
+      </Auxiliary>
+    );
+    let appThumbnails = (
+      <Auxiliary>
+        <li
+          className={classes.App}
+          onClick={this.showAlarmHandler}
+          style={{
+            backgroundColor: this.state.alarm ? "#444b61" : null
+          }}
+        >
+          Alarm
+            </li>
+        <li
+          className={classes.App}
+          onClick={this.showStopwatchHandler}
+          style={{
+            backgroundColor: this.state.stopwatch ? "#444b61" : null
+          }}
+        >
+          Stopw
+            </li>
+        <li
+          className={classes.App}
+          onClick={this.showTimerHandler}
+          style={{
+            backgroundColor: this.state.timer ? "#444b61" : null
+          }}
+        >
+          Timer
+            </li>
+      </Auxiliary>
+    )
     return (
-      <div className={this.props.visible ? classes.box : classes.hide}>
-        <Backdrop visible={this.props.visible} clicked={this.props.zoomOut} />
-        <div className={classes.Watch}>
-          <Clock
-            size={700}
-            hourFormat="roman"
-            timeFormat="standard"
-            boxClassName={classes.ClockBox}
-            className={classes.Clock}
-          />
-          <div className={classes.Time}>
-            <h2>{this.state.currentTime}</h2>
-          </div>
-        </div>
-        <div className={classes.Apps} style={{ color: "white" }}>
-          <ul>
-            <li
-              className={classes.App}
-              onClick={this.showAlarmHandler}
-              style={{
-                backgroundColor: this.state.alarm ? "#444b61" : null
-              }}
-            >
-              Alarm
-            </li>
-            <li
-              className={classes.App}
-              onClick={this.showStopwatchHandler}
-              style={{
-                backgroundColor: this.state.stopwatch ? "#444b61" : null
-              }}
-            >
-              Stopw
-            </li>
-            <li
-              className={classes.App}
-              onClick={this.showTimerHandler}
-              style={{
-                backgroundColor: this.state.timer ? "#444b61" : null
-              }}
-            >
-              Timer
-            </li>
-          </ul>
-          <div
-            className={
-              this.state.alarm || this.state.stopwatch || this.state.timer ? classes.AppViewer : classes.hide
-            }
-          >
-            <Alarm visible={this.state.alarm} />
-            <Stopwatch visible={this.state.stopwatch} />
-            <Timer visible={this.state.timer} />
-          </div>
-        </div>
-      </div>
+      <Applications
+        visible={this.props.visible}
+        zoomOut={this.props.zoomOut}
+        zoomIn={this.props.zoomIn}
+        appClass={classes.Watch}
+        appSvg={appSvg}
+        appThumbnails={appThumbnails}
+        viewAppsCondition={!this.state.alarm && !this.state.stopwatch && !this.state.timer}
+      >
+        <Alarm visible={this.state.alarm} />
+        <Stopwatch visible={this.state.stopwatch} />
+        <Timer visible={this.state.timer} />
+      </Applications>
     );
   }
 }
